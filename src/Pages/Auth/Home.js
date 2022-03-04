50a0ffce7609183470ab95f48053c461cab53c85
Sricharan Ramesh <charan1952001@gmail.com>
2022-03-04 09:07:28 +0530

    feat: add routing

--- src/Pages/Auth/Home.js
+++ src/Pages/Auth/Home.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { authentication } from '../../firebase'
 
 const Home = () => {
   const provider = new GoogleAuthProvider()
-  const auth = getAuth()
 
   const signInWithFirebase = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(authentication, provider)
       .then((res) => {
         const credential = GoogleAuthProvider.credentialFromResult(res)
         const token = credential.accessToken
@@ -14,11 +14,11 @@ const Home = () => {
         console.log(token)
         console.log(user)
       }).catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        const email = error.email;
-        const credential = GoogleAuthProvider.credentialFromError(error);
-
+        const errorCode = error.code
+        const errorMessage = error.message
+        const email = error.email
+        const credential = GoogleAuthProvider.credentialFromError(error)
+        console.log(errorCode, errorMessage, email, credential)
       })
   }
 
